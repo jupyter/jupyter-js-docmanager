bbb3b3f015174e76db3848ec64b305995864fe16
Steven Silvester <steven.silvester@ieee.org>
2016-02-04 14:10:04 -0600

    Add front matter

---
+++ example/webpack.conf.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  entry: './example/build/index.js',
+  output: {
+    filename: './example/build/bundle.js'
+  },
+  node: {
+    fs: "empty"
+  },
+  bail: true,
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ]
+  },
+  resolve: {
+    alias: {
+      'requirejs': 'requirejs/require'
+    }
+  }
+}
